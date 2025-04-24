3b719d00872e619772aa6ccb1f895e6cca008dc7
Leo Birken <leobirken@Beos-MacBook-Pro-2.local>
2025-04-24 08:38:32 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,7 @@
+mix_of_stuff = ["Goat", 700, 99.9, False]
+
+playlist_for_coding = ["And Bird are Still", "Wet Hands", "Vogel im Kafig", "NEVERLAST", "Good Night, Dear Heart"]
+print(playlist_for_coding[0])
+
+playlist_for_coding.append("Never Gonna Give You Up")
+print(playlist_for_coding)
